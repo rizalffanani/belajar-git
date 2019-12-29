d62b1902a3bdd627a1b7d79bf835e842a4480ac7
rffanani <rizalffanani@gmail.com>
2019-12-29 18:04:10 +0700

    update2

--- jam.php
+++ jam.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>MEMBUAT JAM DIGITAL SENDIRI</title>
+<title>MEMBUAT IGITAL SENDIRI</title>
 <?php echo(1+3); ?>
 <script type="text/javascript">
  window.onload = function() { jam(); }
@@ -25,7 +25,8 @@
 </script>
 </head>
 <body>
-<p>Hello Semua, Saya sedang belajar Git</p>
+<p>Hello Semua !,, Saya sedang belajar Git</p>
+<p>Belajar git ternyata cukup menyenangkan</p>
 <center>
 <h4 style="font-size: 20px; font-family: verdana;" id="jam"></h4>
 </center>
